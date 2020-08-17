8ea8232278301a1e69ab18d6d43cfe0108c32601
Matthew Hallberg <matthewhallberg@gmail.com>
2020-08-17 01:54:20 -0400

    handled connection error states, closed socket on photoshop document closed.

--- PhotoshopPlugin/main.js
+++ PhotoshopPlugin/main.js
@@ -5,50 +5,79 @@
 //close port
 //sudo lsof -i :2222
 //sudo kill -9 PID
+
+//connection documentation https://photoshop-connection.readthedocs.io/en/latest/modules/photoshop.html
+
 (function () {
 
 	"use strict";
 
 	var generator;
+	var recieveMessage;
 
 	function init(gen) {
 		generator = gen;
-		generator.addMenuItem("color", "Change Color", true, false);
 		generator.addMenuItem("connection", "Open Connection", true, false);
-		generator.onPhotoshopEvent("generatorMenuChanged", menuClicked);
+		generator.addMenuItem("color", "Change Color", true, false);
+		generator.onPhotoshopEvent("generatorMenuChanged", OnMenuClicked);
+		generator.onPhotoshopEvent("closedDocument", OnDocumentClosed);
 	}
 
-	function menuClicked(e){
-		if (e.generatorMenuChanged.name == "color"){
-			console.log("MENU CLICKED");
-			var str = "var color = app.foregroundColor; \
-						color.rgb.red = " + Math.floor(Math.random() * 255) + "; \
-						color.rgb.green = " + Math.floor(Math.random() * 255) + "; \
-						color.rgb.blue = " + Math.floor(Math.random() * 255) + "; \
-						app.foregroundColor = color;";
-			generator.evaluateJSXString(str);
-		} else if (e.generatorMenuChanged.name == "connection"){
-			//start child process to recieve messages TODO: CLOSE CONNECTION IDIOT
-			var cp = require('child_process');
-			const path = require('path');
-			const dirPath = path.join(__dirname, '/RecieveMessage.js');
-			console.log(dirPath);
-			var n = cp.fork(dirPath);
+	function ChangeRandomColor(){
+		console.log("MENU CLICKED");
+		var str = "var color = app.foregroundColor; \
+			color.rgb.red = " + Math.floor(Math.random() * 255) + "; \
+			color.rgb.green = " + Math.floor(Math.random() * 255) + "; \
+			color.rgb.blue = " + Math.floor(Math.random() * 255) + "; \
+			app.foregroundColor = color;";
+		generator.evaluateJSXString(str);
+	}
+
+	function MoveImage(amount){
+		var str = 
+			"activeDocument.suspendHistory('', ''), sTT = stringIDToTypeID; \
+			(ref = new ActionReference()).putProperty(sTT('historyState'), sTT('currentHistoryState')); \
+			(dsc = new ActionDescriptor()).putReference(sTT('null'), ref), executeAction(sTT('delete'), dsc); \
+			var doc = app.activeDocument; \
+			for (var m = 0; m < doc.layers.length; m++){ \
+			var theLayer = doc.layers[m]; \
+			doc.activeLayer = theLayer; \
+			theLayer.translate(0," + parseFloat(amount) + "); \
+			}";
+		generator.evaluateJSXString(str);
+	}
+
+	function StartChildProcess(script){
+		//start child process to recieve messages TODO: CLOSE CONNECTION IDIOT
+		var cp = require('child_process');
+		const path = require('path');
+		const dirPath = path.join(__dirname, script);
+		return cp.fork(dirPath);
+	}
+
+	function OnConnectButtonPressed(){
+		if (recieveMessage == null){
+			recieveMessage = StartChildProcess("RecieveMessage.js");
 			//listen for messages from child process
-			n.on('message', function(m) {
-				var str = 
-				"activeDocument.suspendHistory('', ''), sTT = stringIDToTypeID; \
-				(ref = new ActionReference()).putProperty(sTT('historyState'), sTT('currentHistoryState')); \
-				(dsc = new ActionDescriptor()).putReference(sTT('null'), ref), executeAction(sTT('delete'), dsc); \
-				var doc = app.activeDocument; \
-				for (var m = 0; m < doc.layers.length; m++){ \
-				var theLayer = doc.layers[m]; \
-				doc.activeLayer = theLayer; \
-				theLayer.translate(0," + parseFloat(m) + "); \
-				}";
-			  console.log(parseFloat(m));
-			  generator.evaluateJSXString(str);
+			recieveMessage.on('message', function(m) {
+				MoveImage(m);
 			});
+		}
+	}
+
+	function OnDocumentClosed(e){
+		if (recieveMessage != null){
+			recieveMessage.kill();
+			recieveMessage = null;
+			console.log("killed message script");
+		}
+	}
+
+	function OnMenuClicked(e){
+		if (e.generatorMenuChanged.name == "color"){
+			ChangeRandomColor();
+		} else if (e.generatorMenuChanged.name == "connection"){
+			OnConnectButtonPressed();
 		}
 	}
 
