a43b8eb8e87064d1941a8d665c317d6776e9339b
Matthew Hallberg <matthewhallberg@gmail.com>
2020-09-24 00:20:04 -0400

    added image tracking with ARfoundation.

---
+++ Assets/Scripts/Permissions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+
+public class Permissions : MonoBehaviour {
+
+    void Awake() {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera)) {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+#endif
+    }
+}
